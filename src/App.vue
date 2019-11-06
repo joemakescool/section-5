1f9f5f93206801b72e42be5dccb883639d779142
joebarker <joebarker@utah.gov>
2019-11-06 09:19:04 -0700

    Lifecycles in Vue js, They are hooks!!

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <template id="thing">
       <h1>{{ title }}</h1>
+      <button @click="title = 'Changed'">Update Title</button>
+      <button @click="destroy">Destroy</button>
     </template>
 
   </div>
@@ -10,15 +12,65 @@
 
 <script>
 
-  setTimeout(()=> {
-    console.log('Joe\'s message')
-  }, 3000);
 
 export default {
   name: 'app',
   components: {
 
+  },
+
+  data: () => ({
+    title: 'Joe\'s life cycle!'
+
+  }),
+
+  beforeCreate: function() {
+    // eslint-disable-next-line no-console
+    console.log('beforeCreate()')
+  },
+
+  created: function () {
+    // eslint-disable-next-line no-console
+    console.log('created()')
+  },
+
+  beforeMount: function () {
+    // eslint-disable-next-line no-console
+    console.log('beforeMount()')
+  },
+
+  mounted: function() {
+    // eslint-disable-next-line no-console
+    console.log('mounted()')
+  },
+
+  beforeUpdate: function() {
+    // eslint-disable-next-line no-console
+    console.log('beforeUpdate()')
+  },
+
+  updated: function() {
+    // eslint-disable-next-line no-console
+    console.log('updated()')
+  },
+
+  beforeDestroy: function() {
+    // eslint-disable-next-line no-console
+    console.log('beforeDestroy()')
+  },
+
+  destoryed: function() {
+    // eslint-disable-next-line no-console
+    console.log('destroyed()')
+  },
+
+  methods: {
+    destroy: function () {
+      this.$destroy();
+    }
   }
+
+
 }
 </script>
 
